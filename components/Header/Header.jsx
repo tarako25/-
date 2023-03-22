000ac0267fd4fe17a67754975797356d2dfd758c
tarako25 <takenoko1603@gmail.com>
2023-03-23 02:53:36 +0900

    ハンバーガーメニュー追加

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -7,48 +7,86 @@ function Header() {
 
 //スクロール時の処理(header)//
   useEffect(() => {
-    const header = document.getElementById("header");
-    const scrollHandler = () => {
-      const cscroll = window.pageYOffset;
-      if (0 < cscroll) {
-        header.classList.add(styles.show);
-      } else {
-        header.classList.remove(styles.show);
-      }
-    };
-    window.addEventListener("scroll", scrollHandler);
-    return () => window.removeEventListener("scroll", scrollHandler);
-  }, []);
+  const header = document.getElementById("header");
 
+  const scrollHandler = () => {
+    const cscroll = window.pageYOffset;
+    if (0 < cscroll) {
+      header.classList.add(styles.show);
+    } else {
+      header.classList.remove(styles.show);
+    }
+  };
+
+  window.addEventListener("scroll", scrollHandler);
+  return () => window.removeEventListener("scroll", scrollHandler);
+}, []);
+
+useEffect(() => {
+  const menus = document.getElementById("menus");
+  const menubox = document.getElementById("menubox");
+
+  const openHandle =() => {
+    menubox.classList.toggle(styles.opened);
+  }
+  menus.addEventListener("click", openHandle );
+  return () => menus.addEventListener("click", openHandle );
+}, );
   return (
-    <div className={styles.header} id="header">
-          <div className={styles.headerlogo}>
-              <p>SampleText</p>
-          </div>
-          <nav className={styles.headernavi}>
-              <ul>
-                  <Link href="">
-                  <li>Company</li>
-                  </Link>
-                  <Link href="">
-                  <li>Service</li>
-                  </Link>
-                  <Link href="">
-                  <li>Career</li>
-                  </Link>
-                  <Link href="">
-                  <li>Acces</li>
-                  </Link>
-                  <Link href="">
-                  <li>Recruit</li>
-                  </Link>
-                  <Link href="">
-                  <li>Contact</li>
-                  </Link>
-              </ul>
-          </nav>
-          <DensityMediumIcon className={styles.icon}/>
-    </div>
+    <>
+      <div className={styles.header} id="header">
+            <div className={styles.headerlogo}>
+                <p>SampleText</p>
+            </div>
+            <nav className={styles.headernavi}>
+                <ul>
+                    <Link href="">
+                    <li>Company</li>
+                    </Link>
+                    <Link href="">
+                    <li>Service</li>
+                    </Link>
+                    <Link href="">
+                    <li>Career</li>
+                    </Link>
+                    <Link href="">
+                    <li>Acces</li>
+                    </Link>
+                    <Link href="">
+                    <li>Recruit</li>
+                    </Link>
+                    <Link href="">
+                    <li>Contact</li>
+                    </Link>
+                </ul>
+            </nav>
+            <div className={styles.iconmain} id="menus">
+              <DensityMediumIcon className={styles.iconsub}/>
+            </div>
+      </div>
+      <div className={styles.menubox} id="menubox">
+        <ul>
+                    <Link href="">
+                    <li>Company</li>
+                    </Link>
+                    <Link href="">
+                    <li>Service</li>
+                    </Link>
+                    <Link href="">
+                    <li>Career</li>
+                    </Link>
+                    <Link href="">
+                    <li>Acces</li>
+                    </Link>
+                    <Link href="">
+                    <li>Recruit</li>
+                    </Link>
+                    <Link href="">
+                    <li>Contact</li>
+                    </Link>
+        </ul>
+      </div>
+    </>
   )
 }
 
